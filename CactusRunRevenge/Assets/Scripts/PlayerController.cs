4c19be00cd5478505990619385a0519dc29c3847
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-01-27 14:36:36 -0800

    Player Update
    Basic player movement script.

---
+++ CactusRunRevenge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D playerRB;
+
+    public bool grounded;
+    public LayerMask whatisGround;
+
+    public Collider2D playerCollider;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(playerCollider, whatisGround);
+        playerRB.velocity = new Vector2(movementSpeed, playerRB.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(grounded)
+            {
+                Jump();
+            }
+
+        }
+    }
+
+    void Jump()
+    {
+        playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+    }
+}
